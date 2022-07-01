e99692faa2dae96a7f2de18871f1e63e113bd44a
NikishaPatil <nipatil@redhat.com>
2022-07-01 11:05:36 +0530

    added files

---
+++ src/components/Shopping.js
@@ -0,0 +1,16 @@
+
+import list from "../data";
+import Cards from "./Card";
+import "../styles/shopping.css";
+
+const Shopping = ({ handleClick }) => {
+  return (
+    <section>
+      {list.map((item) => (
+        <Cards key={item.id} item={item} handleClick={handleClick} />
+      ))}
+    </section>
+  );
+};
+
+export default Shopping;
